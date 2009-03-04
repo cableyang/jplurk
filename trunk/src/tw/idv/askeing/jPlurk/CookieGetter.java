57a60215d8a9844d689851865d11c4e0da83d305
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-03-04 12:47:50 +0000

    use logger instead of System.out.println()

--- trunk/src/tw/idv/askeing/jPlurk/CookieGetter.java
+++ trunk/src/tw/idv/askeing/jPlurk/CookieGetter.java
@@ -8,11 +8,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import tw.idv.askeing.jPlurk.model.AccountModel;
 
 /**
@@ -21,6 +25,8 @@ import tw.idv.askeing.jPlurk.model.AccountModel;
  * @version 1.0
  */
 public class CookieGetter {
+
+	static Log logger = LogFactory.getLog(CookieGetter.class);
 
     /**
      * Return Cookie of user.
@@ -32,7 +38,7 @@ public class CookieGetter {
     public static String getCookie(String host, String postUrl, AccountModel user, String optional_cookie) {
         boolean forTest = false;
         String cookie = "";
-        
+
         try {
             HttpClient client = new HttpClient();
             client.getHostConfiguration().setHost(host, 80, "http");
@@ -52,11 +58,11 @@ public class CookieGetter {
             }else {
                 post.addParameter(new NameValuePair("Cookie", optional_cookie));
             }
-            
+
             // 發送請求、返回狀態
             int statusCode = client.executeMethod(post);
             if (statusCode == HttpStatus.SC_MOVED_TEMPORARILY || statusCode == HttpStatus.SC_OK) {
-                
+
                 // 取得回傳資訊.
                 Header[] headers = post.getResponseHeaders();
                 for(int i=1 ; i<headers.length ; i++) {
@@ -74,14 +80,16 @@ public class CookieGetter {
                     }
                 }
             } else {
-                System.err.println("Method failed: " + post.getStatusLine());
+            	logger.warn("Method failed: " + post.getStatusLine());
+//                System.err.println("Method failed: " + post.getStatusLine());
             }
         } catch (IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
+        	logger.error(e.getMessage(), e);
         }
         return cookie;
     }
-    
+
     /**
      * Test Case
      * @param args the command line arguments
@@ -90,7 +98,7 @@ public class CookieGetter {
         String host = "www.plurk.com";
         String postUrl_1 = "/Users/login";
         String postUrl_2 = "/m/login";
-        
+
         AccountModel user = new AccountModel();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please input your name: ");
@@ -99,7 +107,7 @@ public class CookieGetter {
         System.out.print("Please input your password: ");
         user.setPassword( scanner.next() );
         System.out.println("Password:"+user.getPassword());
-        
+
         System.out.println("\n===== Test One =====\n");
         System.out.println(postUrl_1+" Cookie: "+ CookieGetter.getCookie(host, postUrl_1, user, null) );
         System.out.println("\n===== Test Two =====\n");
