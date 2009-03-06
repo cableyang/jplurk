d5a462cb4bc8b85411249f2f451bb5a44535a8f4
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-03-06 09:52:50 +0000

    prepare enum qualifier

---
+++ jPlurk/src/tw/idv/askeing/jPlurk/Qualifier.java
@@ -0,0 +1,77 @@
+package tw.idv.askeing.jPlurk;
+
+public enum Qualifier {
+
+	// ":" 無. 同 "QUALIFIER_FREESTYLE".
+	NULL(":"),
+
+	// "wants" 想要
+	WANTS("wants"),
+
+	// "needs" 需要
+	NEEDS("needs"),
+
+	// "says" 說
+	SAYS("says"),
+
+	// "asks" 問
+	ASKS("asks"),
+
+	// "wonders" 好奇
+	WONDERS("wonders"),
+
+	// "freestyle" 自由發揮
+	FREESTYLE("freestyle"),
+
+	// "is" 正在
+	IS("is"),
+
+	// "shares" 推
+	SHARES("shares"),
+
+	// "will" 打算
+	WILL("will"),
+
+	// "feels" 覺得
+	FEELS("feels"),
+
+	// "wishes" 希望
+	WISHES("wishes"),
+
+	// "likes" 喜歡
+	LIKES("likes"),
+
+	// "loves" 愛
+	LOVES("loves"),
+
+	// "hopes" 希望
+	HOPES("hopes"),
+
+	// "has" 已經
+	HAS("has"),
+
+	// "was" 曾經
+	WAS("was"),
+
+	// "thinks" 想
+	THINKS("thinks"),
+
+	// "hates" 討厭
+	HATES("hates"),
+
+	// "gives" 給
+	GIVES("gives")
+	;
+
+	private String qualifier;
+
+	Qualifier(String qualifier){
+		this.qualifier = qualifier;
+	}
+
+	@Override
+	public String toString() {
+		return qualifier;
+	}
+
+}
