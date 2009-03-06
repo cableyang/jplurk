f17ef6e66d283ec4d95a43750d9388c0bf1b21dc
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-03-06 10:47:08 +0000

    1. 提出一些常數 2. 去除 man-in-middle method

--- jPlurk/src/tw/idv/askeing/jPlurk/Constants.java
+++ jPlurk/src/tw/idv/askeing/jPlurk/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
 	public final static String PLURK_HOST = "www.plurk.com";
 	public final static int PLURK_PORT = 80;
 
+	public final static String LOGIN_URL = "/Users/login";
+    public final static String POST_URL = "/TimeLine/addPlurk";
+
 }
