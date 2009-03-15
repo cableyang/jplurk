93d9e0600ebf5dc5b7475fc0d255d6326dd169f6
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-03-15 11:25:43 +0000

    remove unused methods

--- jPlurk/test/tw/idv/askeing/jPlurk/PicManagerTest.java
+++ jPlurk/test/tw/idv/askeing/jPlurk/PicManagerTest.java
@@ -5,37 +5,15 @@
 
 package tw.idv.askeing.jPlurk;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Askeing, Yen
  */
 public class PicManagerTest {
-
-    public PicManagerTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
 
     /**
      * Test of getPicByName method, of class PicManager.
