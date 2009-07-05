ee1b3da8556c6c74332ebc58761be42631ce02a5
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-07-05 04:30:56 +0000

    add some utils

--- jPlurk/src/tw/idv/askeing/jPlurk/util/PatternUtils.java
+++ jPlurk/src/tw/idv/askeing/jPlurk/util/PatternUtils.java
@@ -16,4 +16,26 @@ public class PatternUtils {
 		return "";
 	}
 
+	/**
+	 * the posted field in the plurk json response use <b>new Date("Sun, 05 Jul 2009 02:56:30 GMT")</b> define the date, use replaceJsDateToTimestamp method can transform it to timestamp
+	 * @param input
+	 * @return
+	 */
+	public static String replaceJsDateToTimestamp(String input){
+		Matcher m = Pattern.compile("(new Date\\([^(]+\\))", Pattern.DOTALL | Pattern.MULTILINE).matcher(input);
+		m.reset();
+        boolean result = m.find();
+        StringBuffer sb = new StringBuffer();
+        if (result) {
+            do {
+            	System.out.println(m.group(1));
+                m.appendReplacement(sb, "" + TimeUtil.fromJsDate(m.group(1)).getTime());
+                result = m.find();
+            } while (result);
+            m.appendTail(sb);
+        }
+        return sb.toString();
+	}
+
+
 }
