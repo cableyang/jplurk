f957066b8b26d55b9c800ef89a920e4b7015d7c5
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-10-14 06:58:11 +0000

    write js date parsing method instead of js engine

--- trunk/jPlurk/src/com/googlecode/jplurk/PlurkAgentUsageExample.java
+++ trunk/jPlurk/src/com/googlecode/jplurk/PlurkAgentUsageExample.java
@@ -13,15 +13,9 @@ public class PlurkAgentUsageExample {
 
 		Result result = // agent.getUnreadPlurks();
 //			agent.getNotifications();
-			//agent.getPlurks();
-			agent.addPlurk(Qualifier.WANTS, "不不不不不想要");
-		System.out.println(result.getAttachement().get("plurk_id"));
-		
-		Thread.sleep(1000 * 30);
-		
-		agent.editPlurk(""+result.getAttachement().get("plurk_id"), "想要");
-//		System.out.println(result.getAttachement());
+			agent.getPlurks();
 
+		System.out.println(result);
 //		List<Integer> uids = (List<Integer>) result.getAttachement().get("uids");
 //		result = agent.allowFriendRequest(uids.get(0));
 //		System.out.println(result);
