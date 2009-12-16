2ff5f10e61c45537dd22068d7b0a847bf5ebc09f
chingyichan.tw@gmail.com <chingyichan.tw@gmail.com@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-12-16 09:50:20 +0000

    implement login action

---
+++ trunk/com.google.jplurk/src/main/java/com/google/jplurk/PlurkClient.java
@@ -0,0 +1,79 @@
+package com.google.jplurk;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.jplurk.action.PlurkActionSheet;
+import com.google.jplurk.exception.PlurkException;
+
+public class PlurkClient {
+	PlurkSettings config;
+
+	private ResponseHandler responseHandler = new BasicResponseHandler();
+
+	private String getApiUri(String uri) {
+		return "http://www.plurk.com/API" + uri;
+	}
+
+	public PlurkClient(PlurkSettings settings) {
+		this.config = settings;
+	}
+
+
+
+	public JSONObject login(String user, String password) throws ClientProtocolException, IOException {
+
+		try {
+			HttpGet method = (HttpGet) PlurkActionSheet.getInstance().login(
+				config.createParamMap()
+					.k("username").v(user)
+					.k("password").v(password)
+					.getMap()
+			);
+
+			JSONObject ret = new JSONObject(execute(method));
+			return ret;
+		} catch (PlurkException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	private String execute(HttpRequestBase method) throws PlurkException{
+		HttpClient client = new DefaultHttpClient();
+		String result = null;
+		try {
+			result = (String) client.execute(method, responseHandler);
+		} catch (Exception e) {
+			throw new PlurkException(e);
+		}
+		client.getConnectionManager().shutdown();
+		return result;
+	}
+
+	public static void main(String[] args) throws PlurkException, ClientProtocolException, IOException {
+		PlurkClient pc = new PlurkClient(new PlurkSettings());
+		JSONObject o = pc.login(JOptionPane.showInputDialog("id"), JOptionPane.showInputDialog("password"));
+		System.out.println(o);
+	}
+
+
+}
---
+++ trunk/com.google.jplurk/src/main/java/com/google/jplurk/PlurkSettings.java
@@ -0,0 +1,121 @@
+package com.google.jplurk;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.jplurk.exception.PlurkException;
+
+/**
+ * PlurkSettings class will read a property file which contains plurk api key at least.
+ *
+ * @author qty
+ */
+public class PlurkSettings {
+
+	public final static String settings = "jplurk.properties";
+	private Properties prop;
+
+	/**
+	 * Create settings object from ~/jplurk.properties file
+	 *
+	 * @throws PlurkException
+	 */
+	public PlurkSettings() throws PlurkException {
+		prop = getSettings();
+	}
+
+	/**
+	 * Create settings object from given file object.
+	 *
+	 * @param settingFile
+	 * @throws PlurkException
+	 */
+	public PlurkSettings(File settingFile) throws PlurkException {
+		prop = getSettings(settingFile);
+	}
+
+	/**
+	 * @return api key read from settings file
+	 */
+	public String getApiKey() {
+		return prop.getProperty("api_key");
+	}
+
+	/**
+	 * @return default user read from settings file
+	 */
+	public String getDefaultUser() {
+		return prop.getProperty("default_user");
+	}
+
+	/**
+	 * @return default password read from settings file
+	 */
+	public String getDefaultPassword() {
+		return prop.getProperty("default_password");
+	}
+
+	public MapHelper createParamMap() {
+		return new MapHelper(new HashMap<String, String>() {
+			private static final long serialVersionUID = -5306686629368927936L;
+			{
+				put("api_key", getApiKey());
+			}
+		});
+	}
+
+	public MapHelper createParamMapWithDefaultUserInfo(){
+		return createParamMap()
+			.k("username").v(getDefaultUser())
+			.k("password").v(getDefaultPassword());
+	}
+
+	private Properties getSettings() throws PlurkException {
+		File dir = new File(System.getProperty("user.home", "."));
+		dir.mkdirs();
+		File setting = new File(dir, settings);
+		return getSettings(setting);
+	}
+
+	private Properties getSettings(File settingFile) throws PlurkException {
+		Properties prop = new Properties();
+		if (!settingFile.exists()) {
+			throw new PlurkException("settings file is not found: "
+					+ settingFile.getAbsolutePath());
+		}
+
+		FileInputStream fin = null;
+		try {
+			fin = new FileInputStream(settingFile);
+			prop.load(fin);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fin != null) {
+				try {
+					fin.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+
+		if (StringUtils.isBlank(prop.getProperty("api_key"))) {
+			throw new PlurkException("settings has no api_key.");
+		}
+
+		return prop;
+	}
+
+	public static void main(String[] args) throws PlurkException {
+		PlurkSettings p = new PlurkSettings();
+		System.out.println(p);
+	}
+}
