da48014bfb810cb15d37af3b23256656c21b2303
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-12-28 02:15:52 +0000

    change return value

--- com.google.jplurk/src/main/java/com/google/jplurk/net/JPlurkResponseHandler.java
+++ com.google.jplurk/src/main/java/com/google/jplurk/net/JPlurkResponseHandler.java
@@ -11,15 +11,15 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 
+public class JPlurkResponseHandler implements ResponseHandler<Result> {
 
-public class JPlurkResponseHandler implements ResponseHandler<String> {
+	static Logger logger = org.slf4j.LoggerFactory
+			.getLogger(JPlurkResponseHandler.class);
 
-	static Logger logger = org.slf4j.LoggerFactory.getLogger(JPlurkResponseHandler.class);
-
-    public String handleResponse(final HttpResponse response)
-            throws HttpResponseException, IOException {
-    	HttpEntity entity = response.getEntity();
-    	String ret = (entity == null ? null : EntityUtils.toString(entity));
+	public Result handleResponse(final HttpResponse response)
+			throws HttpResponseException, IOException {
+		HttpEntity entity = response.getEntity();
+		String ret = (entity == null ? null : EntityUtils.toString(entity));
 
 		if (logger.isDebugEnabled()) {
 			Header[] headers = response.getAllHeaders();
@@ -29,16 +29,16 @@ public class JPlurkResponseHandler implements ResponseHandler<String> {
 			logger.info(response.getStatusLine().toString());
 		}
 
-        StatusLine statusLine = response.getStatusLine();
-        if (statusLine.getStatusCode() >= 300) {
-        	logger.warn("Http Response Body: \n" + ret);
-            throw new HttpResponseException(statusLine.getStatusCode(),
-                    statusLine.getReasonPhrase());
-        }
-        
-        logger.debug("Response: " + ret);
+		StatusLine statusLine = response.getStatusLine();
+		if (statusLine.getStatusCode() >= 300) {
+			logger.warn("Http Response Body: \n" + ret);
+			return new Result(ret, false);
+		}
 
-        return ret;
-    }
+		if (logger.isDebugEnabled()) {
+			logger.debug("Response: " + ret);
+		}
+		return new Result(ret, true);
+	}
 
 }
