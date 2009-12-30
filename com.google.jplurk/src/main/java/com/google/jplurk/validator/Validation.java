dfd2302136e4d99e7842089949dfce80cddb7cd3
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2009-12-30 06:15:08 +0000

    refactoring: move validator

--- com.google.jplurk/src/main/java/com/google/jplurk/validator/Validation.java
+++ com.google.jplurk/src/main/java/com/google/jplurk/validator/Validation.java
@@ -1,11 +1,10 @@
-package com.google.jplurk.action;
+package com.google.jplurk.validator;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.google.jplurk.validator.IValidator;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
