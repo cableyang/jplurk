06c2aa4b54a4634f0ea49d0f69ddbd9cf7d7027e
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2010-01-01 13:15:01 +0000

    add simple settings

--- trunk/com.google.jplurk/src/main/java/com/google/jplurk/ISettings.java
+++ trunk/com.google.jplurk/src/main/java/com/google/jplurk/ISettings.java
@@ -1,5 +1,7 @@
 package com.google.jplurk;
 
+import java.util.HashMap;
+
 public interface ISettings {
 
 	public abstract String getApiKey();
@@ -15,5 +17,46 @@ public interface ISettings {
 	public abstract MapHelper createParamMap();
 
 	public abstract String getLang();
+
+	static class Simple implements ISettings {
+
+		private String apiKey;
+		private Lang lang;
+
+		public Simple(String apiKey, Lang lang) {
+			this.apiKey = apiKey;
+			this.lang = (lang == null ? Lang.en : lang);
+		}
+
+		public MapHelper createParamMap() {
+			MapHelper m = new MapHelper(new HashMap<String, String>());
+			m.k("api_key").v(getApiKey());
+			return m;
+		}
+
+		public String getApiKey() {
+			return apiKey;
+		}
+
+		public String getDefaultProxyHost() {
+			return null;
+		}
+
+		public String getDefaultProxyPassword() {
+			return null;
+		}
+
+		public int getDefaultProxyPort() {
+			return 0;
+		}
+
+		public String getDefaultProxyUser() {
+			return null;
+		}
+
+		public String getLang() {
+			return lang.toString();
+		}
+	}
 
 }
