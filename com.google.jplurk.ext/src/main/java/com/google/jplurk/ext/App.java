ba98f46db8b9aaa382410b559cfa37226652b3dc
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2010-01-09 15:53:58 +0000

    write a simple app to test plurk

--- com.google.jplurk.ext/src/main/java/com/google/jplurk/ext/App.java
+++ com.google.jplurk.ext/src/main/java/com/google/jplurk/ext/App.java
@@ -1,13 +1,21 @@
 package com.google.jplurk.ext;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javax.swing.JOptionPane;
+
+import org.json.JSONObject;
+
+import com.google.jplurk.ISettings;
+import com.google.jplurk.PlurkClient;
+import com.google.jplurk.PlurkSettings;
+import com.google.jplurk.exception.PlurkException;
+
+public class App {
+	public static void main(String[] args) throws PlurkException {
+		ISettings settings = new PlurkSettings();
+		PlurkClient client = new PlurkClient(settings);
+		JSONObject o = client.login(JOptionPane.showInputDialog("id"),
+				JOptionPane.showInputDialog("password"));
+		System.out.println(o);
+
+	}
 }
