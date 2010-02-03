0b00cb2ac54bad3199b744e3d6e63fc86d61e57b
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2010-02-03 03:24:56 +0000

    refactoring the DateTime

--- com.google.jplurk/src/main/java/com/google/jplurk/DateTime.java
+++ com.google.jplurk/src/main/java/com/google/jplurk/DateTime.java
@@ -4,155 +4,165 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+
 
 public class DateTime {
 
 	private static Log logger = LogFactory.getLog(DateTime.class);
 
-	public final static SimpleDateFormat OFFSET_OUTPUT_FORMAT =
-		new SimpleDateFormat("yyyy-M-d'T'HH:mm:ss", Locale.US);
+    public final static SimpleDateFormat OFFSET_OUTPUT_FORMAT = createFormat("yyyy-M-d'T'HH:mm:ss");
+    public final static SimpleDateFormat JS_INPUT_FORMAT = createFormat("E, dd MMM yyyy HH:mm:ss z");
+    public final static SimpleDateFormat BIRTHDAY_FORMAT = createFormat("yyyy-MM-dd");
 
-	public final static SimpleDateFormat JS_INPUT_FORMAT =
-		new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.US);
+    private static SimpleDateFormat createFormat(String pattern){
+        SimpleDateFormat fmt = new SimpleDateFormat(pattern, Locale.US);
+        fmt.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return fmt;
+    }
+    
+    protected static class TimeUtil {
+        public static void setYear(Calendar time, int year) {
+            time.set(Calendar.YEAR, year);
+        }
 
-	private int year;
-	private int month;
-	private int day;
-	private int hour;
-	private int minute;
-	private int second;
+        public static void setMonth(Calendar time, int month) {
+            time.set(Calendar.MONTH, month -1);
+        }
 
-	private DateTime(int year, int month, int day) {
-		this.year = year;
-		this.month = month;
-		this.day = day;
-	}
+        public static void setDate(Calendar time, int date) {
+            time.set(Calendar.DATE, date);
+        }
 
-	private DateTime(int year, int month, int day,
-		int hour, int minute, int second) {
+        public static void setHour(Calendar time, int hour) {
+            time.set(Calendar.HOUR_OF_DAY, hour);
+        }
 
-		this.year = year;
-		this.month = month;
-		this.day = day;
-		this.hour = hour;
-		this.minute = minute;
-		this.second = second;
-	}
+        public static void setMinute(Calendar time, int minute) {
+            time.set(Calendar.MINUTE, minute);
+        }
+        public static void setSecond(Calendar time, int second) {
+            time.set(Calendar.SECOND, second);
+        }
+    }
 
-	public static DateTime create(long offset){
-		Date date = new Date();
-		date = new Date(date.getTime() + offset);
-		Calendar calendar = Calendar.getInstance();
-		calendar.clear();
-		calendar.setTime(date);
-		return create(calendar);
-	}
+	private Calendar time = createGmtCalendar();
 
-	public static DateTime create(String offset){
-		DateTime dateTime = null;
-		Date date = null;
+    private static Calendar createGmtCalendar() {
+        return Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+    }
 
-		try {
-			date = OFFSET_OUTPUT_FORMAT.parse(offset);
-			logger.info("create date time from[" + offset + "] by: "
-					+ OFFSET_OUTPUT_FORMAT);
-			logger.info("date is " + date);
-		} catch (Exception e) {
-			logger.debug(e.getMessage());
-		}
+    private DateTime(int year, int month, int day) {
+        time.clear();
+        TimeUtil.setYear(time, year);
+        TimeUtil.setMonth(time, month);
+        TimeUtil.setDate(time, day);
+    }
 
-		if (date == null) {
-			try {
-				date = JS_INPUT_FORMAT.parse(offset);
-				logger.info("create date time from[" + offset + "] by: "
-						+ JS_INPUT_FORMAT);
-				logger.info("date is " + date);
-			} catch (Exception e) {
-				logger.debug(e.getMessage());
-			}
-		}
+    private DateTime(int year, int month, int day, int hour, int minute, int second) {
+        this(year, month, day);
+        TimeUtil.setHour(time, hour);
+        TimeUtil.setMinute(time, minute);
+        TimeUtil.setSecond(time, second);
+    }
+    
+    private DateTime(Calendar time){
+        this.time = time;
+    }
+    
+    private DateTime(Date time){
+        this.time.setTime(time);
+    }
 
-		if(date == null){
-			logger.warn("cannot parse date string: " + offset + ". we will use now instead");
-			return DateTime.now();
-		}
+    public static DateTime create(long offset) {
+        Calendar calendar = createGmtCalendar();
+        Date date = new Date(calendar.getTimeInMillis() + offset);
+        calendar.setTime(date);
+        return create(calendar);
+    }
+    
+    public static DateTime create(Calendar calendar){
+        logger.info("create DateTime from Calendar: " + calendar.getTime());
+        return new DateTime(calendar);
+    }
 
-		try {
-			Calendar c = Calendar.getInstance();
-			c.clear();
-			c.setTime(date);
-			dateTime = create(c);
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			dateTime = now();
-		}
+    public static DateTime create(String offset) {
+        DateTime dateTime = null;
+        Date date = null;
 
-		logger.info("create date time from string: " + dateTime.toTimeOffset());
-		return dateTime;
-	}
+        try {
+            date = OFFSET_OUTPUT_FORMAT.parse(offset);
+            logger.info("create date time from[" + offset + "] by: "
+                    + OFFSET_OUTPUT_FORMAT);
+            logger.info("date is " + date);
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+        }
 
-	public static DateTime create(int year, int month, int day){
-		return new DateTime(year, month, day);
-	}
+        if (date == null) {
+            try {
+                date = JS_INPUT_FORMAT.parse(offset);
+                logger.info("create date time from[" + offset + "] by: "
+                        + JS_INPUT_FORMAT);
+                logger.info("date is " + date);
+            } catch (Exception e) {
+                logger.debug(e.getMessage());
+            }
+        }
 
-	public static DateTime create(int year, int month, int day,
-			int hour, int minute, int second) {
-		return new DateTime(year, month, day, hour, minute, second);
-	}
+        if (date == null) {
+            logger.warn("cannot parse date string: " + offset
+                    + ". we will use now instead");
+            return DateTime.now();
+        }
 
-	public static DateTime create(Calendar calendar){
-		logger.info("create DateTime from Calendar: " + calendar.getTime());
-		return new DateTime(
-				calendar.get(Calendar.YEAR),
-				calendar.get(Calendar.MONTH) + 1,
-				calendar.get(Calendar.DAY_OF_MONTH),
-				calendar.get(Calendar.HOUR_OF_DAY),
-				calendar.get(Calendar.MINUTE),
-				calendar.get(Calendar.SECOND));
-	}
+        try {
+            dateTime = new DateTime(date);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            dateTime = now();
+        }
 
-	public static DateTime now() {
-		Calendar c = Calendar.getInstance();
-		c.setTime(new Date());
-		return create(c);
-	}
+        logger.info("create date time from string: " + dateTime.toTimeOffset());
+        return dateTime;
+    }
 
-	protected String birthday() {
-		Calendar c = Calendar.getInstance();
-		c.clear();
-		c.set(Calendar.YEAR, year);
-		c.set(Calendar.MONTH, month - 1);
-		c.set(Calendar.DAY_OF_MONTH, day);
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-		return df.format(c.getTime());
-	}
+    public static DateTime create(int year, int month, int day) {
+        return new DateTime(year, month, day);
+    }
 
-	public String toTimeOffset() {
-		return OFFSET_OUTPUT_FORMAT.format(toCalendar().getTime());
-	}
+    public static DateTime create(int year, int month, int day, int hour,
+            int minute, int second) {
+        return new DateTime(year, month, day, hour, minute, second);
+    }
 
-	public Calendar toCalendar() {
-		Calendar c = Calendar.getInstance();
-		c.clear();
-		c.set(Calendar.YEAR, year);
-		c.set(Calendar.MONTH, month - 1);
-		c.set(Calendar.DATE, day);
-		c.set(Calendar.HOUR_OF_DAY, hour);
-		c.set(Calendar.MINUTE, minute);
-		c.set(Calendar.SECOND, second);
-		return c;
-	}
+    public static DateTime now() {
+        return create(createGmtCalendar());
+    }
 
-	public static boolean isValidTimeOffset(String offset) {
-		try {
-			OFFSET_OUTPUT_FORMAT.parse(offset);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-	}
+    protected String birthday() {
+        return BIRTHDAY_FORMAT.format(time.getTime());
+    }
+
+    public String toTimeOffset() {
+        return OFFSET_OUTPUT_FORMAT.format(toCalendar().getTime());
+    }
+
+    public Calendar toCalendar() {
+        return time;
+    }
+
+    public static boolean isValidTimeOffset(String offset) {
+        try {
+            OFFSET_OUTPUT_FORMAT.parse(offset);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 
 }
