ef42926f24fce9c58a63cd9ed16ebf667258736e
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2010-02-03 03:24:56 +0000

    refactoring the DateTime

--- trunk/com.google.jplurk/src/test/java/com/google/jplurk/PlurkUtilsTest.java
+++ trunk/com.google.jplurk/src/test/java/com/google/jplurk/PlurkUtilsTest.java
@@ -4,12 +4,13 @@ import junit.framework.TestCase;
 
 public class PlurkUtilsTest extends TestCase {
 
-	public void testBirthday() throws Exception {
-		assertEquals("1911-01-01", DateTime.create(1911, 1, 1).birthday());
-	}
+    public void testBirthday() throws Exception {
+        assertEquals("1911-01-01", DateTime.create(1911, 1, 1).birthday());
+    }
 
-	public void testOffset() throws Exception {
-		assertEquals("2009-6-20T21:55:34", DateTime.create(2009, 6, 20, 21, 55, 34).toTimeOffset());
-	}
+    public void testOffset() throws Exception {
+        assertEquals("2009-6-20T21:55:34", DateTime.create(2009, 6, 20, 21, 55,
+                34).toTimeOffset());
+    }
 
 }
