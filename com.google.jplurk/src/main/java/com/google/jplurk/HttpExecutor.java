352f5ef192e3fb6e8ea1c24d48ea5227c05d987f
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2010-08-06 04:36:25 +0000

    move http operations to HttpExecutor

--- com.google.jplurk/src/main/java/com/google/jplurk/HttpExecutor.java
+++ com.google.jplurk/src/main/java/com/google/jplurk/HttpExecutor.java
@@ -4,9 +4,14 @@ import java.net.SocketTimeoutException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
@@ -15,6 +20,7 @@ import com.google.jplurk.exception.PlurkException;
 import com.google.jplurk.net.HttpClientFactory;
 import com.google.jplurk.net.JPlurkResponseHandler;
 import com.google.jplurk.net.LazyIdleConnectionMonitor;
+import com.google.jplurk.net.ProxyProvider;
 import com.google.jplurk.org.apache.commons.lang.StringUtils;
 
 public class HttpExecutor {
@@ -28,6 +34,58 @@ public class HttpExecutor {
 
     public HttpExecutor(ISettings config) {
         this.config = config;
+        ProxyProvider.loadDefaultProxy(config);
+        ProxyProvider.loadDynamicProxy();
+        configureProxy();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    client.getConnectionManager().shutdown();
+                } catch (Exception ignored) {
+                }
+            }
+        });
+    }
+
+    /**
+     * ProgrammaticallySetting > Dynamic > Default. Load Default Proxy Setting,
+     * then load Dynamic Proxy Setting.
+     * */
+    protected void configureProxy() {
+        // Auth Proxy Setting
+        if (StringUtils.isNotBlank(ProxyProvider.getUser())) {
+            ((DefaultHttpClient) client).getCredentialsProvider()
+                    .setCredentials(
+                            new AuthScope(ProxyProvider.getHost(),
+                                    ProxyProvider.getPort()),
+                            new UsernamePasswordCredentials(ProxyProvider
+                                    .getUser(), ProxyProvider.getPassword()));
+            logger.debug("Proxy: Auth "
+                + ((UsernamePasswordCredentials) ((DefaultHttpClient) client)
+                        .getCredentialsProvider().getCredentials(AuthScope.ANY)).getUserName());
+            logger.debug("Proxy: Auth "
+                + ((UsernamePasswordCredentials) ((DefaultHttpClient) client)
+                        .getCredentialsProvider().getCredentials(AuthScope.ANY)).getPassword());
+        }
+        // Proxy Host Setting
+        if (StringUtils.isNotBlank(ProxyProvider.getHost())) {
+            HttpHost proxy = new HttpHost(ProxyProvider.getHost(),
+                    ProxyProvider.getPort());
+            client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
+                    proxy);
+            logger.debug("Proxy: Host "
+                    + ((HttpHost) client.getParams().getParameter(
+                            ConnRoutePNames.DEFAULT_PROXY)).getHostName());
+            logger.debug("Proxy: Prot "
+                    + String.valueOf(((HttpHost) client.getParams()
+                            .getParameter(ConnRoutePNames.DEFAULT_PROXY))
+                            .getPort()));
+        }
+    }
+
+    protected HttpClient getHttpClient() {
+        return client;
     }
 
     @SuppressWarnings("unchecked")
