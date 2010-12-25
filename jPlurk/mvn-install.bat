55ca8adfc298e0eb46a2f636f90f8daf66c363a3
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2010-12-25 03:59:51 +0000

    Implement ISSUE 16: http://code.google.com/p/jplurk/issues/detail?id=16

--- jPlurk/mvn-install.bat
+++
@@ -1,2 +0,0 @@
-cd /d %0\..
-mvn install:install-file -DgroupId=tw.idv.askeing.jPlurk -DartifactId=jplurk.osgi -Dversion=1.0.0 -Dpackaging=jar -Dfile=jplurk-osgi.jar
--- jPlurk/src/com/googlecode/jplurk/behavior/IBehavior.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.jplurk.behavior;
-
-import com.googlecode.jplurk.net.Request;
-
-public interface IBehavior {
-	
-	public boolean action(final Request params, Object arg);
-
-}
