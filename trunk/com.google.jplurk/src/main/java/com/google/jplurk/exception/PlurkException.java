583e99bb59c7a81a63abe0cf98b7b7b4f493a878
ChingYiChan.tw <ChingYiChan.tw@34ad0024-086c-11de-a6e9-d320b29efae0>
2011-01-02 16:25:42 +0000

    merge from /branches/feature_excpetion_in_stead_of_null

--- trunk/com.google.jplurk/src/main/java/com/google/jplurk/exception/PlurkException.java
+++ trunk/com.google.jplurk/src/main/java/com/google/jplurk/exception/PlurkException.java
@@ -1,22 +1,30 @@
 package com.google.jplurk.exception;
 
-public class PlurkException extends Throwable {
+public class PlurkException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public PlurkException() {
+    public PlurkException() {
 
-	}
+    }
 
-	public PlurkException(String message) {
-		super(message);
-	}
+    public PlurkException(String message) {
+        super(message);
+    }
 
-	public PlurkException(Throwable t) {
-		super(t);
-	}
+    public PlurkException(Throwable t) {
+        super(t);
+    }
 
-	public PlurkException(String message, Throwable t) {
-		super(message, t);
-	}
+    public PlurkException(String message, Throwable t) {
+        super(message, t);
+    }
+    
+    public static PlurkException create(Throwable e)
+    {
+        if (e instanceof PlurkException) {
+            return (PlurkException) e;
+        }
+        return new PlurkException(e);
+    }
 }
--- trunk/com.google.jplurk/src/main/java/com/google/jplurk/net/JPlurkResponseHandler.java
+++ trunk/com.google.jplurk/src/main/java/com/google/jplurk/net/JPlurkResponseHandler.java
@@ -12,31 +12,31 @@ import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.util.EntityUtils;
 
-
 public class JPlurkResponseHandler implements ResponseHandler<String> {
 
-	private static Log logger = LogFactory.getLog(JPlurkResponseHandler.class);
+    private static Log logger = LogFactory.getLog(JPlurkResponseHandler.class);
 
     public String handleResponse(final HttpResponse response)
             throws HttpResponseException, IOException {
-    	HttpEntity entity = response.getEntity();
-    	String ret = (entity == null ? null : EntityUtils.toString(entity));
-
-		if (logger.isDebugEnabled()) {
-			Header[] headers = response.getAllHeaders();
-			for (Header header : headers) {
-				logger.debug("Response Header: " + header.toString());
-			}
-			logger.info(response.getStatusLine().toString());
-		}
+        HttpEntity entity = response.getEntity();
+        String ret = (entity == null ? null : EntityUtils.toString(entity));
+        
+        if (logger.isDebugEnabled()) {
+            Header[] headers = response.getAllHeaders();
+            for (Header header : headers) {
+                logger.debug("Response Header: " + header.toString());
+            }
+            logger.info(response.getStatusLine().toString());
+        }
+        logger.info("HttpContent: " + ret);
 
         StatusLine statusLine = response.getStatusLine();
         if (statusLine.getStatusCode() >= 300) {
-        	logger.warn("Http Response Body: \n" + ret);
+            logger.warn("Http Response Body: \n" + ret);
             throw new HttpResponseException(statusLine.getStatusCode(),
                     statusLine.getReasonPhrase());
         }
-        
+
         logger.debug("Response: " + ret);
 
         return ret;
